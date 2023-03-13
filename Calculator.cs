325a0edb64bb4f4f477ea93e42893d68e608fafe
Skajjal01 <mahsa@students.amikom.ac.id>
2023-03-13 17:08:32 +0700

    Melengkapi Project

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,22 @@ class Calculator
 {
 	static void main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 		
+		console.WriteLine("Hasli penjumlahan : {0} + {1} = {2}", a, b, Penambahan(a, b));
+		console.WriteLine("Hasli pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		console.WriteLine("\nTekan sembarang key untuk keluar");
+		console.Readkey();
 	}
+	
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 }
